051c6a390ca437477b4d33e7a38dea2c988f9e54
nafisalawalidris <nafisaidris413@gmail.com>
2024-08-20 23:42:43 +0100

    updated

--- main2.py
+++ main2.py
@@ -9,9 +9,14 @@ def root():
     # Returning a JSON response with a "message" key and "Hello World" as the value
     return {"message": "Hello World"}
 
-
 # Define a route for the "/posts" path
 @app.get("/posts")
 def get_posts():
     # Return a JSON response with placeholder data for posts
     return {"data": "This is your posts"}
+
+# Define a route for the "/createposts" path
+@app.post("/createposts")
+def create_posts():
+    # Return a JSON response indicating successful post creation
+    return {"message": "successfully created posts"}
